4da065141fe1e2df67758d4358921460a7f59115
lujingxiao <policeschool@126.com>
2024-08-26 09:56:10 +0800

    feat: 添加Mybatis源码解析工具

--- mybatis/src/main/java/com/policeschool/code/gzqhero/domain/Product.java
+++ mybatis/src/main/java/com/policeschool/code/gzqhero/domain/Product.java
@@ -1,10 +1,14 @@
 package com.policeschool.code.gzqhero.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     private Long id;
     private String name;
--- mybatis/src/test/java/com/policeschool/code/yuandongli/MyBatisToolTest.java
+++ mybatis/src/test/java/com/policeschool/code/yuandongli/MyBatisToolTest.java
@@ -1,6 +1,10 @@
 package com.policeschool.code.yuandongli;
 
+import com.policeschool.code.gzqhero.domain.OrderItem;
+import com.policeschool.code.gzqhero.domain.Product;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.builder.StaticSqlSource;
+import org.apache.ibatis.builder.xml.XMLConfigBuilder;
 import org.apache.ibatis.builder.xml.XMLMapperEntityResolver;
 import org.apache.ibatis.datasource.pooled.PooledDataSource;
 import org.apache.ibatis.logging.slf4j.Slf4jImpl;
@@ -8,20 +12,29 @@ import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlCommandType;
 import org.apache.ibatis.mapping.SqlSource;
+import org.apache.ibatis.ognl.Ognl;
+import org.apache.ibatis.ognl.OgnlContext;
+import org.apache.ibatis.ognl.OgnlException;
 import org.apache.ibatis.parsing.XNode;
 import org.apache.ibatis.parsing.XPathParser;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.apache.ibatis.type.TypeAliasRegistry;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
  * MyBatis中常用的工具类的使用测试
  */
+@Slf4j
 public class MyBatisToolTest {
 
     /**
@@ -65,5 +78,45 @@ public class MyBatisToolTest {
         configuration.addMappedStatement(mappedStatement);
 
         System.out.println(configuration);
+    }
+
+    @Test
+    public void testXMLConfigBuilder() throws IOException {
+        InputStream inputStream = new ClassPathResource("mybatis-config.xml").getInputStream();
+        XMLConfigBuilder xmlConfigBuilder = new XMLConfigBuilder(inputStream);
+        Configuration parse = xmlConfigBuilder.parse();
+        log.info("parse: {}", parse);
+    }
+
+    @Test
+    public void textOGNL() throws OgnlException {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setOrderId(1L);
+        orderItem.setId(1L);
+        orderItem.setAmount(BigDecimal.ONE);
+        orderItem.setPrice(BigDecimal.TEN);
+        orderItem.setProduct(new Product(1L, "洗发露", "desc", BigDecimal.TEN));
+
+        OgnlContext context = new OgnlContext(null, null, new DefaultMemberAccess(false));
+        Object value = Ognl.getValue("product.name", context, orderItem);
+        log.info("product.name -> {}", value);
+
+        Object res1 = Ognl.getValue("product.name == '洗发露'", context, orderItem);
+        log.info("product.name == '洗发露' -> {}", res1);
+
+        Object res2 = Ognl.getValue("product.name == null", context, orderItem);
+        log.info("product.name == null -> {}", res2);
+
+        // 方法调用
+        context.put("orderItem", orderItem);
+        Object res3 = Ognl.getValue("#orderItem.product.name.substring(1,2)", context, new Object());
+        log.info("res3 -> {}", res3);
+    }
+
+    @Test
+    public void testTypeAliasRegistry() {
+        TypeAliasRegistry typeAliasRegistry = new TypeAliasRegistry();
+        typeAliasRegistry.registerAlias("orderItem", OrderItem.class);
+        typeAliasRegistry.registerAliases("com.policeschool.code.gzqhero.domain");
     }
 }
